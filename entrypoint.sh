d9424623352a967f7f5bfdaaceac915ff345234d
NateMooreMon <natemoore@monday.com>
2025-02-20 09:00:24 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
-
 TOKEN=$1
 APP_ID=$2
 REGION=$3
+VERSION_ID=$4
+FORCE=$5
 
 mapps init -t $TOKEN
 
-if [ -n "$REGION" ]; then
-  mapps code:push -a $APP_ID -z $REGION
+# Build command arguments conditionally
+if [ -z "$APP_ID" ]; then
+  APP_ID_ARG=""
 else
-  mapps code:push -a $APP_ID
+  APP_ID_ARG="-a $APP_ID"
 fi
+
+if [ -z "$REGION" ]; then
+  REGION_ARG=""
+else
+  REGION_ARG="-z $REGION"
+fi
+
+if [ -z "$VERSION_ID" ]; then
+  VERSION_ID_ARG=""
+else
+  VERSION_ID_ARG="-i $VERSION_ID"
+fi
+
+if [ -z "$FORCE" ]; then
+  FORCE_ARG=""
+else
+  FORCE_ARG="-f"
+fi
+
+mapps code:push $APP_ID_ARG $REGION_ARG $VERSION_ID_ARG $FORCE_ARG
